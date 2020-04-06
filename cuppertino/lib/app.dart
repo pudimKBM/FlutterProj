9615d8fa6701c2edac4a9a37b02a427fffdb7c21
AntonioSut <antonio@suthub.com>
2020-04-05 23:48:49 -0300

    commitada violenta pt2

---
+++ cuppertino/lib/app.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'styles.dart';
+import 'product_list_tab.dart';
+import 'shopping_cart_tab.dart';
+import 'seach_tab.dart';
+
+class CupertinoStoreApp extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context){
+    return CupertinoApp(
+      home: CupertinoStoreHomepage(),
+    );
+  } 
+}
+
+class CupertinoStoreHomepage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return CupertinoTabScaffold(
+      tabBar : CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            title: Text('Products'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            title: Text('Search')
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            title: Text('Cart')
+          ),
+        ],
+      ),
+      tabBuilder: (context, index){
+        switch(index){
+          case 0 :
+          return CupertinoTabView(builder: (context){
+            return CupertinoPageScaffold(
+              child: ProductListTab(),
+            );
+          });
+          case 1 :
+          return CupertinoTabView(builder : (context) {
+            return CupertinoPageScaffold(
+              child: SearchTab(), 
+              );
+          });
+          case 2 :
+          return CupertinoTabView(builder : (context){
+            return CupertinoPageScaffold(
+              child: ShoppingCartTab(),
+              );
+          });
+        }
+      },
+
+      // navigationBar: CupertinoNavigationBar(
+      //   middle: const Text('cuppertino Store'),
+      //   ),
+      //   child: Container(),
+      );
+  }
+}
